515d89185c88959e827578e452f5f06050871fb6
Jace P. Gold <Jace@JaceGold.com>
2017-11-13 00:56:19 -0700

    Formatted the headings and seperated default responses into default_responses.rb to avoid accidental deletion

--- magic_eight.rb
+++ magic_eight.rb
@@ -23,22 +23,23 @@
 
 require 'pry'
 require 'rainbow'
+require_relative 'default_responses'
 
-# Default answers source: https://en.wikipedia.org/wiki/Magic_8-Ball
-# default_answers array is a restore point.
-@default_answers = [
-  'It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
-  'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again',
-  'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
-  'Don\t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'
-]
+# # Default answers source: https://en.wikipedia.org/wiki/Magic_8-Ball
+# # default_answers array is a restore point.
+# @default_answers = [
+#   'It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
+#   'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again',
+#   'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
+#   'Don\t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'
+# ]
 # Clone the default answers. Use this array to add more answers from the user
 @answers = @default_answers.clone
 
 
 # Show all answers from @answers array of answers
 def show_answers
-  puts '----- All The Answers -----'
+  puts "\n\t----- All The Answers -----\n"
   @answers.each do |answer|
     puts answer
   end
@@ -46,6 +47,7 @@ end
 
 # The actual game
 def magic_eight
+  puts "\n\t------ MAGIC 8 BALL ------\n"
   puts "Type a question to get an answer, or type QUIT to end the program."
   print '> '
   question = $stdin.gets.strip
@@ -109,14 +111,14 @@ end
 
 # Main easter_egg_menu
 def easter_egg_menu
-   puts '----- Easter Egg Menu! Much more options to explore -----'
+   puts "\n\t----- Easter Egg Menu! Much more options to explore -----\n"
    puts '1.) Magic 8'
    puts '2.) Add Answers'
    puts '3.) Show Answers'
    puts '4.) Reset answers'
    puts '5.) Quit'
    print '> '
-   choice = $stdin.gets.strip
+   choice = $stdin.gets.strip.downcase
    case choice
    when '1'
      magic_eight
